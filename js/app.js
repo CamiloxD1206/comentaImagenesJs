70a5afb82b3b41e2f2dcfb70a998bb25ebfdc09b
camilo <cetacue@misena.edu.co>
2023-08-09 16:09:25 -0500

    se agrego otro boton de like pero infinito

--- js/app.js
+++ js/app.js
@@ -2,17 +2,16 @@
   const formulario = document.querySelector('#formulario');
   const listaTweets = document.querySelector('#lista-tweets');
   const imagenInput = document.querySelector('#imagenInput');
-  let comen = []; // Arreglo para almacenar los tweets (comentarios) y sus imágenes
+  let comen = [];
 
-  eventlistener(); // Asignar los eventos a los elementos del formulario
+  eventlistener(); 
 
   function eventlistener() {
-      // Evento para agregar un nuevo tweet cuando se envía el formulario
+     
       formulario.addEventListener('submit', agregartweet);
 
-      // Evento que se dispara cuando el DOM ha sido completamente cargado
       document.addEventListener('DOMContentLoaded', () => {
-          // Obtener los tweets almacenados en el LocalStorage al cargar la página
+         
           comen = JSON.parse(localStorage.getItem('comen')) || [];
           console.log(comen);
           // Mostrar los tweets en la página
@@ -21,59 +20,71 @@
   }
 
   function agregartweet(e) {
-      e.preventDefault(); // Evitar el comportamiento predeterminado del formulario (recargar la página)
+      e.preventDefault(); 
 
-      // Obtener el texto del tweet ingresado por el usuario
+
       const tweets = document.querySelector('#tweet').value;
 
-      // Validar si el tweet está vacío
+ 
       if (tweets === '' && !imagenInput.files[0]) {
           mostrarerror('El mensaje y/o img está vacío  ');
       }
 
 
-      // Obtener la imagen seleccionada por el usuario
+  
       const imagen = imagenInput.files[0];
-      // Crear una URL que representa la imagen
+  
       const imagenUrl = URL.createObjectURL(imagen);
 
-      // Crear un objeto que representa el nuevo tweet con su imagen
+      
       const tweetobj = {
-          id: Date.now(), // ID único para el tweet
-          tweets: tweets, // Texto del tweet
-          imagenUrl: imagenUrl // URL de la imagen
+          id: Date.now(),
+          tweets: tweets, 
+          imagenUrl: imagenUrl 
       };
-      comen = [...comen, tweetobj]; // Agregar el tweet al arreglo comen
-      // Actualizar la lista de tweets en la página
+      comen = [...comen, tweetobj]; 
+     
       crearHTML();
-      formulario.reset(); // Limpiar el formulario después de agregar el tweet
+      formulario.reset(); 
   }
 
   function mostrarerror(error) {
-      // Mostrar un mensaje de error en la página
+   
       const mensajeError = document.createElement('p');
       mensajeError.textContent = error;
-      mensajeError.classList.add('error'); // Agregar una clase para estilizar el mensaje
+      mensajeError.classList.add('error'); 
       const contenido = document.querySelector('#contenido');
       contenido.appendChild(mensajeError);
 
-      // Eliminar el mensaje de error después de 3 segundos (3000 milisegundos)
+    
       setTimeout(() => {
           mensajeError.remove();
       }, 3000);
   }
 
   function crearHTML() {
-      limpiarHTMl(); // Limpiar la lista de tweets en la página
+      limpiarHTMl(); 
       if (comen.length > 0) {
-          // Mostrar los tweets en la lista de tweets
+    
           comen.forEach((tweet) => {
-              // Crear un botón para eliminar el tweet
+          
+            //-------------contador infinito---------------------
+                let conador=0;
+                const botonn=document.createElement('button');
+                botonn.classList.add('like-tweet')
+                botonn.textContent=`Likes`;
+                botonn.addEventListener('click',()=>{
+                    conador++;
+                    botonn.textContent=`Likes:${conador}`;
+                    botonn.style.backgroundColor='rgb(22, 150, 236)'
+                })
+                //--------------------------------------
+
               const botonBorrar = document.createElement('button');
               botonBorrar.classList = 'borrar-tweet';
               botonBorrar.innerText = 'Eliminar';
               botonBorrar.onclick = () => {
-                  // Llamar a la función para eliminar el tweet
+               
                   borrarTweet(tweet.id);
               };
               //crear un boton para me gusta el tweet
@@ -93,49 +104,51 @@
                       botoNMegusta.innerText = 'likes';
 
                   }
+          
 
 
               })
+      
+     
+        
 
               function reiniciarContador() {
                   contador = 0;
               }
-              // Crear un elemento <div> que contiene el texto del tweet y la imagen
+             
               const div = document.createElement('div');
               div.classList.add('div-imagen')
               div.textContent = tweet.tweets;
 
-              // Agregar la imagen al elemento <div>
               const imagen = document.createElement('img');
               imagen.src = tweet.imagenUrl;
-              imagen.width = 200; // Definir el ancho de la imagen (ajústalo según tus necesidades)
-
-              // Agregar el botón de eliminar al elemento <li>
+              imagen.width = 200; 
+             
               div.appendChild(imagen);
               div.appendChild(botonBorrar);
-              div.appendChild(botoNMegusta)
+              div.appendChild(botoNMegusta);
+              div.appendChild(botonn);
               listaTweets.appendChild(div);
           });
       }
-      agregarstorage(); // Actualizar el LocalStorage con los tweets actuales
-  }
+      agregarstorage();
 
   function agregarstorage() {
-      // Guardar el arreglo comen en el LocalStorage en formato JSON
+    
       localStorage.setItem('comen', JSON.stringify(comen));
   }
 
   function borrarTweet(id) {
       console.log('Eliminando tweet', id);
-      // Filtrar el arreglo comen para eliminar el tweet con el ID especificado
+     
       comen = comen.filter((tweet) => tweet.id !== id);
       // Actualizar la lista de tweets en la página después de eliminar el tweet
       crearHTML();
   }
 
   function limpiarHTMl() {
-      // Limpiar la lista de tweets en la página
+    
       while (listaTweets.firstChild) {
           listaTweets.removeChild(listaTweets.firstChild);
       }
-  }
+  }}
